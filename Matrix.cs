96f747ea990180b2cd60891f0ec743616d6b1f8b
Michael Rawlings <mrawlingst@gmail.com>
2017-01-20 10:13:09 -0500

    Matrix class with +

---
+++ Matrix.cs
@@ -0,0 +1,54 @@
+﻿namespace CSC316_Assignment1
+{
+    public class Matrix
+    {
+        private int rows, cols;
+        private float[][] data;
+
+        public Matrix()
+        {
+            rows = cols = 4;
+            data = new float[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                data[i] = new float[cols];
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (i == j)
+                        data[i][j] = 1.0f;
+                    else
+                        data[i][j] = 0.0f;
+                }
+            }
+        }
+
+        public static Matrix operator+(Matrix A, Matrix B)
+        {
+            return new Matrix();
+        }
+
+        public static Matrix operator-(Matrix A, Matrix B)
+        {
+            return new Matrix();
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    output += data[i][j] + "\t";
+                }
+
+                output += "\n";
+            }
+
+            return output;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSC316_Assignment1
 {
@@ -10,6 +6,8 @@ namespace CSC316_Assignment1
     {
         static void Main(string[] args)
         {
+            Matrix A = new Matrix();
+            Console.WriteLine(A.ToString());
         }
     }
 }
