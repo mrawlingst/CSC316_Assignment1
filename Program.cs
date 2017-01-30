453f72d862f6dda7b5cd47f922ff904ab7a8095b
Michael Rawlings <mrawlingst@gmail.com>
2017-01-30 11:40:58 -0500

    regex for all functions - NOT IN CORRECT ORDER

--- Program.cs
+++ Program.cs
@@ -40,6 +40,54 @@ namespace CSC316_Assignment1
 
                 if (Regex.Matches(expressionInput, @"q(uit)?$").Count > 0)
                     break;
+
+                Matrix resultMatrix = new Matrix();
+                MatchCollection matches;
+
+                // Translate
+                matches = Regex.Matches(expressionInput, @"T\((([+-])?(0|([1-9][0-9]*))(\.[0-9]+)?(\,)?){3}\)");
+                foreach (var match in matches)
+                {
+                    MatchCollection numbers = Regex.Matches(match.ToString(), @"([+-])?(0|([1-9][0-9]*))(\.[0-9]+)?");
+                    resultMatrix = resultMatrix * Matrix.Translate(float.Parse(numbers[0].Value), float.Parse(numbers[1].Value), float.Parse(numbers[2].Value));
+                }
+
+                // Rotate X
+                matches = Regex.Matches(expressionInput, @"RX\((([+-])?(0|([1-9][0-9]*))(\.[0-9]+)?(\,)?){1}\)");
+                foreach (var match in matches)
+                {
+                    MatchCollection numbers = Regex.Matches(match.ToString(), @"([+-])?(0|([1-9][0-9]*))(\.[0-9]+)?");
+                    resultMatrix = resultMatrix * Matrix.RotateX(float.Parse(numbers[0].Value));
+                }
+
+                // Rotate Y
+                matches = Regex.Matches(expressionInput, @"RY\((([+-])?(0|([1-9][0-9]*))(\.[0-9]+)?(\,)?){1}\)");
+                foreach (var match in matches)
+                {
+                    MatchCollection numbers = Regex.Matches(match.ToString(), @"([+-])?(0|([1-9][0-9]*))(\.[0-9]+)?");
+                    resultMatrix = resultMatrix * Matrix.RotateX(float.Parse(numbers[0].Value));
+                }
+
+                // Rotate Z
+                matches = Regex.Matches(expressionInput, @"RZ\((([+-])?(0|([1-9][0-9]*))(\.[0-9]+)?(\,)?){1}\)");
+                foreach (var match in matches)
+                {
+                    MatchCollection numbers = Regex.Matches(match.ToString(), @"([+-])?(0|([1-9][0-9]*))(\.[0-9]+)?");
+                    resultMatrix = resultMatrix * Matrix.RotateX(float.Parse(numbers[0].Value));
+                }
+
+                // Scale
+                matches = Regex.Matches(expressionInput, @"S\((([+-])?(0|([1-9][0-9]*))(\.[0-9]+)?(\,)?){1}\)");
+                foreach (var match in matches)
+                {
+                    MatchCollection numbers = Regex.Matches(match.ToString(), @"([+-])?(0|([1-9][0-9]*))(\.[0-9]+)?");
+                    resultMatrix = resultMatrix * Matrix.RotateX(float.Parse(numbers[0].Value));
+                }
+
+                if (Regex.Match(expressionInput, @"\*M").Success)
+                    resultMatrix = resultMatrix * inputMatrix;
+
+                Console.WriteLine(resultMatrix.ToString());
             }
         }
     }
